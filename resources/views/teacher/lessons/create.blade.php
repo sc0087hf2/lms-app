49f3a512b8a5affdfa7c602ea29704de4ec882a9
yajima <gangshishidao39@gmail.com>
2024-10-16 03:51:30 +0900

    軽微修正

--- resources/views/teacher/lessons/create.blade.php
+++ resources/views/teacher/lessons/create.blade.php
@@ -59,7 +59,7 @@
           <!-- 備考 -->
           <div class="p-2 mb-8 w-full">
             <div class="relative">
-              <label for="teacher_comment" class="leading-7 text-sm text-gray-600">備考</label>
+              <label for="teacher_comment" class="leading-7 text-sm text-gray-600">授業後フィードバック</label>
               <textarea id="teacher_comment" name="teacher_comment" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ old('comment') }}</textarea>
             </div>
           </div>
